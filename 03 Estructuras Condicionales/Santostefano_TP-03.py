16c2c795d71eaecf9cc6d2044ec5b2f7e9e405f9
jpsantostefano <jpsantostefano@gmail.com>
2025-08-25 16:02:48 -0300

    Ejercicio 9 hecho

--- 03 Estructuras Condicionales/Santostefano_TP-03.py
+++ 03 Estructuras Condicionales/Santostefano_TP-03.py
@@ -120,3 +120,29 @@ elif numero == "3":
 else:
     print("Ingrese una opcion valida")
 
+# Ejercicio 9:
+# Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la magnitud en una de las siguientes categorías según la escala de Richter e imprima el resultado por pantalla:
+# ● Menor que 3: "Muy leve" (imperceptible).
+# ● Mayor o igual que 3 y menor que 4: "Leve" (ligeramente perceptible).
+# ● Mayor o igual que 4 y menor que 5: "Moderado" (sentido por personas, pero generalmente no causa daños).
+# ● Mayor o igual que 5 y menor que 6: "Fuerte" (puede causar daños en estructuras débiles).
+# ● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
+# ● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).
+
+magnitud = int(input("Ingrese la magnitud de un terremoto: "))
+
+if magnitud < 3:
+    print("Muy leve (imperceptible)")
+elif magnitud >= 3 and magnitud < 4:
+    print("Leve (Ligeramente perceptible)")
+elif magnitud >= 4 and magnitud < 5:
+    print("Moderado (Sentido por personas, pero generalmente no causa danos)")
+elif magnitud >= 5 and magnitud < 6:
+    print("Fuerte (puede causar danos en estructuras debiles)")
+elif magnitud >= 6 and magnitud < 7:
+    print("Muy fuerte (puede causar danos significativos)")
+elif magnitud >= 7:
+    print("Extremo (puede causar graves danos a gran escala)")
+else:
+    print("No es una magnitud valida")
+
