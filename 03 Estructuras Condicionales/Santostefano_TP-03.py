e672f325454e6240d33b58dfc5e6af967416dfb6
jpsantostefano <jpsantostefano@gmail.com>
2025-08-25 14:47:31 -0300

    Ejercicio 4 hecho

--- 03 Estructuras Condicionales/Santostefano_TP-03.py
+++ 03 Estructuras Condicionales/Santostefano_TP-03.py
@@ -28,3 +28,24 @@ if numero % 2 == 0:
     print("Ha ingresado un numero par")
 else:
     print("Por favor, ingrese un numero par")
+
+# Ejercicio 4:
+# Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las siguientes categorías pertenece: 
+# ● Niño/a: menor de 12 años. 
+# ● Adolescente: mayor o igual que 12 años y menor que 18 años. 
+# ● Adulto/a joven: mayor o igual que 18 años y menor que 30 años. 
+# ● Adulto/a: mayor o igual que 30 años.
+
+edad = int(input("Ingrese su edad: "))
+
+if edad < 12:
+    print("Usted es Niño/a.")
+elif edad >= 12 and edad < 18:
+    print("Usted es Adolescente.")
+elif edad >= 18 and edad < 30:
+    print("Usted es Adulto/a joven.")
+else:
+    print("Usted es Adulto/a:")
+
+
+
