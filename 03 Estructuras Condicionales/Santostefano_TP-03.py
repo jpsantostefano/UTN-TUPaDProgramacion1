f3fc10ba3ec9c7d9864ee8c63c7ab2244da4acea
jpsantostefano <jpsantostefano@gmail.com>
2025-08-25 14:39:52 -0300

    Ejercicio 3 hecho

--- 03 Estructuras Condicionales/Santostefano_TP-03.py
+++ 03 Estructuras Condicionales/Santostefano_TP-03.py
@@ -11,8 +11,20 @@ if edad > 18:
 # en caso contrario deberá mostrar el mensaje “Desaprobado”.
 
 nota = int(input("Ingrese su nota: "))
+
 if nota >= 6:
     print("Aprobado")
 else:
     print("Desaprobado")
-    
+
+# Ejercicio 3:
+# Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; 
+# en caso contrario, imprimir por pantalla "Por favor, ingrese un número par". 
+# Nota: investigar el uso del operador de módulo (%) en Python para evaluar si un número es par o impar.
+
+numero = int(input("Ingrese un numero: "))
+
+if numero % 2 == 0:
+    print("Ha ingresado un numero par")
+else:
+    print("Por favor, ingrese un numero par")
