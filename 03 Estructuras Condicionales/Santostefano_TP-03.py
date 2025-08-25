f65a40d45f3f6167bbd52f83cfcc9442d54c79ac
jpsantostefano <jpsantostefano@gmail.com>
2025-08-25 15:51:33 -0300

    Ejercicio 8 hecho

--- 03 Estructuras Condicionales/Santostefano_TP-03.py
+++ 03 Estructuras Condicionales/Santostefano_TP-03.py
@@ -98,3 +98,25 @@ else:
     print(frase)
 
 # Ejercicio 8:
+# Escribir un programa que solicite al usuario que ingrese su nombre y el número 1, 2 o 3 dependiendo de la opción que desee:
+# 1. Si quiere su nombre en mayúsculas. Por ejemplo: PEDRO.
+# 2. Si quiere su nombre en minúsculas. Por ejemplo: pedro.
+# 3. Si quiere su nombre con la primera letra mayúscula. Por ejemplo: Pedro.
+# El programa debe transformar el nombre ingresado de acuerdo a la opción seleccionada por el usuario e imprimir el resultado por pantalla. 
+# Nota: investigue uso de las funciones upper(), lower() y title() de Python para convertir entre mayúsculas y minúsculas.
+
+nombre = input("Ingrese su nombre: ")
+print("Ingrese 1 si quiere su nombre en mayusculas")
+print("Ingrese 2 si quiere su nombre en minusculas")
+print("Ingrese 3 si quiere su nombre con la primera letra en mayusculas")
+numero = input()
+
+if numero == "1":
+    print(nombre.upper())
+elif numero == "2":
+    print(nombre.lower())
+elif numero == "3":
+    print(nombre.title())
+else:
+    print("Ingrese una opcion valida")
+
