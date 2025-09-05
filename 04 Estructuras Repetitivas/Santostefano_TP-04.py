e8f5a14d03ad2e15487ad52cff407a1a16b4b7d6
jpsantostefano <jpsantostefano@gmail.com>
2025-09-05 15:05:32 -0300

    Ejercicio 6 hecho

--- 04 Estructuras Repetitivas/Santostefano_TP-04.py
+++ 04 Estructuras Repetitivas/Santostefano_TP-04.py
@@ -53,3 +53,9 @@ while random != num:
     intentos = intentos + 1
 
 print("Adiviniaste! Tuviste",intentos,"intentos")
+
+# 6) Desarrolla un programa que imprima en pantalla todos los números pares comprendidos entre 0 y 100, en orden decreciente.
+
+for i in range(100,0,-1):
+    if i % 2 == 0:
+        print(i)
--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-import random
-
-intentos = 0
-random = random.randint(0,9)
-num = ""
-
-while random != num:
-    print("El numero es incorrecto")
-    num = int(input("Ingrese y adivine el numero del 0 al 9: "))
-    intentos = intentos + 1
-
-print("Adiviniaste! Tuviste",intentos,"intentos")
+for i in range(100,0,-1):
+    if i % 2 == 0:
+        print(i)
 
