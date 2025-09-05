9369002e96309ca5470153279b1ad59d90d20cc7
jpsantostefano <jpsantostefano@gmail.com>
2025-09-05 15:21:25 -0300

    Ejercicio 7 hecho

--- 04 Estructuras Repetitivas/Santostefano_TP-04.py
+++ 04 Estructuras Repetitivas/Santostefano_TP-04.py
@@ -59,3 +59,13 @@ print("Adiviniaste! Tuviste",intentos,"intentos")
 for i in range(100,0,-1):
     if i % 2 == 0:
         print(i)
+
+# 7) Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un número entero positivo indicado por el usuario.
+
+suma = 0
+num = int(input("Ingrese un numero entero positivo: "))
+
+for i in range(0,num+1):
+    suma = suma + i
+
+print("La suma de todos los numeros comprendidos entre 0 y",num,"es igual a",suma)
