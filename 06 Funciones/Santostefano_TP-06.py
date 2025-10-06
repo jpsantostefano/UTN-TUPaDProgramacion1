da65ea1ececef8f66c6887f46c45495e2dce8541
jpsantostefano <jpsantostefano@gmail.com>
2025-10-06 12:18:06 -0300

    Ejercicio 3 terminado

--- 06 Funciones/Santostefano_TP-06.py
+++ 06 Funciones/Santostefano_TP-06.py
@@ -4,10 +4,14 @@ def imprimir_hola_mundo():
 
 imprimir_hola_mundo()
 
-# 2. Crear una función llamada saludar_usuario(nombre) que reciba
-# como parámetro un nombre y devuelva un saludo personalizado.
-# Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá devolver: “Hola Marcos!”. Llamar a esta función desde el programa
-# principal solicitando el nombre al usuario.
+# 2. Crear una función llamada saludar_usuario(nombre) que reciba como parámetro un nombre y devuelva un saludo personalizado.
+# Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá devolver: “Hola Marcos!”. Llamar a esta función desde el programa principal solicitando el nombre al usuario.
+def saludar_usuario(nombre):
+    print(f"Hola {nombre}!")
+
+saludar_usuario(input("Ingrese nombre de usuario: "))
+
+
 # 3. Crear una función llamada informacion_personal(nombre, apellido,
 # edad, residencia) que reciba cuatro parámetros e imprima: “Soy
 # [nombre] [apellido], tengo [edad] años y vivo en [residencia]”. Pedir los datos al usuario y llamar a esta función con los valores ingresados.
---
+++ test.py
@@ -0,0 +1,4 @@
+def saludar_usuario(nombre):
+    print(f"Hola {nombre}!")
+
+saludar_usuario(input("Ingrese nombre de usuario: "))
