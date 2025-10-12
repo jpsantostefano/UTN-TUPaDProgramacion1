c797f5200d79989caff6e836ba5e32bca34778f1
jpsantostefano <jpsantostefano@gmail.com>
2025-10-12 20:21:13 -0300

    Ejercicio 4 terminado

--- 07 Estructuras de datos complejas/Santostefano_TP-07.py
+++ 07 Estructuras de datos complejas/Santostefano_TP-07.py
@@ -14,3 +14,18 @@ precios_frutas["Melón"] = 2800
 # Ejercicio 3
 frutas_sin_precio = list(precios_frutas.keys())
 
+# Ejercicio 4
+contactos = {}
+print("Ingrese 5 contactos con su nombre y telefono")
+for i in range(5):
+    nombre = input(f"Nombre del contacto {i+1}: ")
+    contactos[nombre] = None
+    telefono = input("Numero telefonico: ")
+    contactos[nombre] = telefono
+
+nombre = input("Ingresa nombre de contacto para ver si existe: ")
+if nombre in contactos.keys():
+    print(f"El telefono de {nombre} es {contactos[nombre]}")
+else:
+    print("El contacto ingresado no existe")
+
--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-numeros = {1,1,2,3,4,4,4,4,4,5,6,6,6,7,8,8,8,9}
+# Ejercicio 4
+contactos = {}
+print("Ingrese 5 contactos con su nombre y telefono")
+for i in range(5):
+    nombre = input(f"Nombre del contacto {i+1}: ")
+    contactos[nombre] = None
+    telefono = input("Numero telefonico: ")
+    contactos[nombre] = telefono
 
-print(numeros)
+nombre = input("Ingresa nombre de contacto para ver si existe: ")
+if nombre in contactos.keys():
+    print(f"El telefono de {nombre} es {contactos[nombre]}")
+else:
+    print("El contacto ingresado no existe")
