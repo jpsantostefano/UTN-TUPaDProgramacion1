549512e52126bcacb3f7d5656dda29031a8f51ce
jpsantostefano <jpsantostefano@gmail.com>
2025-10-12 20:00:53 -0300

    Ejercicio 2 terminado

--- 07 Estructuras de datos complejas/Santostefano_TP-07.py
+++ 07 Estructuras de datos complejas/Santostefano_TP-07.py
@@ -6,4 +6,9 @@ precios_frutas["Naranja"] = 1200
 precios_frutas["Manzana"] = 1500
 precios_frutas["Pera"] = 2300
 
+# Ejercicio 2
+precios_frutas["Banana"] = 1330
+precios_frutas["Manzana"] = 1700
+precios_frutas["Melón"] = 2800
+
 
