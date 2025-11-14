cd35559648dac8f45010251f47c32bd554c49a08
jpsantostefano <jpsantostefano@gmail.com>
2025-11-14 17:23:04 -0300

    Ejercicio 2 terminado

--- 09 Recursividad/Santostefano_TP-09.py
+++ 09 Recursividad/Santostefano_TP-09.py
@@ -10,3 +10,20 @@ numero = int(input("Ingrese un número: "))
 for i in range(1, numero + 1):
     resultado = factorial(i)
     print(f"Factorial de {i} es: {resultado}")
+
+#Ejercicio 2
+def fibonacci(pos):
+    if pos == 0:
+        return 0
+    elif pos == 1:
+        return 1
+    else:
+        return fibonacci(pos - 1) + fibonacci(pos - 2)
+
+posicion = int(input("Ingrese la posición hasta la que quiera mostrar la serie Fibonacci: "))
+
+print("Serie de Fibonacci completa:")
+for i in range(posicion + 1):
+    resultado = fibonacci(i)
+    print(f"Posición {i}: {resultado}")
+
